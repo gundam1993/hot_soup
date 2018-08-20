281a0147428f4abc3327a1bd682fd42b60238ad4
huangerdong <3498103680@qq.com>
2018-08-20 18:20:24 +0800

    update

---
+++ lib/pages/postList.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PostListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(child: new Text('PostListPage'));
+  }
+}
