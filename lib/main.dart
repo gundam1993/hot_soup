4f66030fdbc4e3c3ddba0284258591d32dde9f06
huangerdong <3498103680@qq.com>
2018-08-22 11:21:59 +0800

    use tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import './pages/postList.dart';
-import './pages/LikeList.dart';
 import './pages/PostDetailPage.dart';
 import './widgets/SideDrawer.dart';
 
@@ -13,78 +12,19 @@ class MyApp extends StatefulWidget {
 }
 
 class ClientState extends State<MyApp> {
-  int _tabIndex = 0;
-  final tabTextStyleNormal = new TextStyle(color: Colors.grey[600]);
-  final tabTextStyleSelected = new TextStyle(color: Colors.red);
-  // 页面底部 TabItem 上的图标数组
-  var tabImages;
   // 页面顶部的大标题
-  var appBarTitles = ['Timeline', 'Collection'];
-  var _body;
+  // var appBarTitles = ['Timeline', 'Collection'];
   // 全局路由
   var _routes;
   // 数据初始化
   void initData() {
-    if (tabImages == null) {
-      tabImages = [
-        [Icon(Icons.home, color: Colors.grey[600]), Icon(Icons.home, color: Colors.red)],
-        [Icon(Icons.star_border, color: Colors.grey[600]), Icon(Icons.star, color: Colors.red)]
-      ];
-    }
     if (_routes == null) {
       _routes = {
         'PostDetail': (BuildContext) {
           return new PostDetailPage();
         },
-        'PostList': (BuildContext) {
-          return new PostListPage();
-        },
-        'LikeList': (BuildContext) {
-          return new LikeListPage();
-        },
       };
     }
-    _body = new IndexedStack(
-      children: <Widget>[
-        new PostListPage(),
-        new LikeListPage()
-      ],
-      index: _tabIndex,
-    );
-  }
-
-  // 根据索引值确定 Tab 是选中状态的样式还是非选中状态的样式
-  TextStyle getTabTextStyle(int curIndex) {
-    if (curIndex == _tabIndex) {
-      return tabTextStyleSelected;
-    }
-    return tabTextStyleNormal;
-  }
-
-  // 根据索引值确定 TabItem 的 icon 是选中还是非选中
-  Icon getTabIcon(int curIndex) {
-    if (curIndex == _tabIndex) {
-      return tabImages[curIndex][1];
-    }
-    return tabImages[curIndex][0];
-  }
-
-  Text getTabTitle(int curIndex) {
-    return new Text(
-      appBarTitles[curIndex],
-      style: getTabTextStyle(curIndex),
-    );
-  }
-
-  List<BottomNavigationBarItem> getBottomNavItems() {
-    List<BottomNavigationBarItem> list = new List();
-    for (int i = 0; i < 2; i++) {
-      list.add(new BottomNavigationBarItem(
-        icon: getTabIcon(i),
-        title: getTabTitle(i)
-      ));
-    }
-    return list;
   }
 
   @override
@@ -104,22 +44,32 @@ class ClientState extends State<MyApp> {
         primarySwatch: Colors.red,
       ),
       routes: _routes,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Hot Soup',style: new TextStyle(color: Colors.white))
-        ),
-        body: _body,
-        bottomNavigationBar: new CupertinoTabBar(
-          items: getBottomNavItems(),
-          currentIndex: _tabIndex,
-          onTap: (index) {
-            // 底部 TabItem 的点击事件处理，改变选中的 tab 的索引值
-            setState(() {
-              _tabIndex = index;
-            });
-          },
-        ),
-        drawer: new SideDrawer(),
+      home: new DefaultTabController(
+        length: 5,
+        child: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('CNODE',style: new TextStyle(color: Colors.white)),
+            bottom: new TabBar(
+              tabs: <Widget>[
+                new Tab(text: '全部',key: new Key('all')),
+                new Tab(text: '精华',key: new Key('good')),
+                new Tab(text: '分享',key: new Key('share')),
+                new Tab(text: '问答',key: new Key('ask')),
+                new Tab(text: '招聘',key: new Key('job')),
+              ],
+            ),
+          ),
+          body: new TabBarView(
+            children: [
+              new PostListPage('all'),
+              new PostListPage('good'),
+              new PostListPage('share'),
+              new PostListPage('ask'),
+              new PostListPage('job'),
+            ]
+          ),
+          drawer: new SideDrawer(),
+        )
       )
     );
   }  
